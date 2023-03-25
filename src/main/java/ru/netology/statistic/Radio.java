751d68d95bf8c6ac0d083848ee5c9c7b7a65867e
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-03-25 23:02:39 +0300

    test radio 2

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -2,21 +2,33 @@ package ru.netology.statistic;
 
 
 public class Radio {
-    private  int maxRadioStation = 9;
-    private  int minRadioStation = 0;
-    private  int currentRadioStation;
-    private  int maxVolume = 100;
-    private  int minVolume = 0;
-    private  int currentVolume;
+    private int maxRadioStation = 9;
+    private int minRadioStation = 0;
+    private int currentRadioStation;
+    private int maxVolume = 100;
+    private int minVolume = 0;
+    private int currentVolume;
 
+
+    public Radio(int maxRadioStation) {
+        this.maxRadioStation = maxRadioStation - 1;
+    }
+
+    public Radio() {
+        this.maxRadioStation = 9;
+
+    }
 
     public int getMaxRadioStation() {
         return maxRadioStation;
     }
 
+
     public int getMinRadioStation() {
         return minRadioStation;
+
     }
+
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
@@ -62,9 +74,11 @@ public class Radio {
         return maxVolume;
     }
 
+
     public int getMinVolume() {
         return minVolume;
     }
+
 
     public int getCurrentVolume() {
         return currentVolume;
